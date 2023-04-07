e34da0473a6637758f3a7e31471058f59c464d75
AlexMNet <emtamaldonado@gmail.com>
2023-04-06 23:11:22 -0700

    add apple touch icons and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,25 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="180x180"
+          href="/icons/apple-180x180-touch-icon.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="120x120"
+          href="/icons/apple-120x120-touch-icon.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="152x152"
+          href="/icons/apple-152x152-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Family Feast"></meta>
       </Head>
       <body>
         <Main />
