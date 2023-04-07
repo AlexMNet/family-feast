f5cc6afe7ad4389c65fc9a51e0e5118dce20fe5e
AlexMNet <emtamaldonado@gmail.com>
2023-04-06 22:26:01 -0700

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        feast:
+          "url('https://images.unsplash.com/photo-1577308856961-8e9ec50d0c67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80')",
       },
     },
   },
   plugins: [],
-}
+};
